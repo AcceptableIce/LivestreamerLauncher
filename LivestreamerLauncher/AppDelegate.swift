1eeee78c08159ffc0117aede6438d8acff721c0b
May Roussel <may@maybreak.com>
2014-08-28 14:58:25 -0500

    Initial Commit

---
+++ LivestreamerLauncher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LivestreamerLauncher
+//
+//  Created by Jake on 8/28/14.
+//  Copyright (c) 2014 Acceptable Ice Development. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LivestreamerLauncher/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  LivestreamerLauncher
+//
+//  Created by Jake on 8/28/14.
+//  Copyright (c) 2014 Acceptable Ice Development. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
